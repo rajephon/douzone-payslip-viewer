40927255314efa6cf94e37ee1fcbfb453086e23f
rajephon <rajephon@gmail.com>
2020-09-19 21:47:03 +0900

    Updates

---
+++ precache-manifest.29f6cc80bf3b9673a6baefb33a175052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93df1c605d93d013fa920d4c55c42d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984dacd7a8fe261b01f8",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "da006288cea6e05d8fe5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "984dacd7a8fe261b01f8",
+    "url": "/static/js/2.8dda5f80.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.8dda5f80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da006288cea6e05d8fe5",
+    "url": "/static/js/main.4d06f53b.chunk.js"
+  },
+  {
+    "revision": "26db8924833949ded0d9",
+    "url": "/static/js/runtime-main.2f1e393f.js"
+  }
+]);
